531acf0035357bee10398d638322cfdf0eb8cc47
Melissa Gould <mag8224@g.rit.edu>
2020-02-16 18:36:21 -0500

    added all tests for library_db_interface

---
+++ tests_data/test_library_db_interface.py
@@ -0,0 +1,16 @@
+EXPECTED = [{'age': 14,
+  'borrowed_books': ['Book'],
+  'fname': 'First',
+  'lname': 'Last',
+  'memberID': 123},
+ {'age': 14,
+  'borrowed_books': ['Book'],
+  'fname': 'Test',
+  'lname': 'Last',
+  'memberID': 1234}]
+
+UPDATED = [{'age': 14,
+  'borrowed_books': ['Book'],
+  'fname': 'Test',
+  'lname': 'LastName',
+  'memberID': 123}]
